d8bb8ce7897172a9d688f01c337c5331a93789d0
MahmudulHassan5809 <mahmudul.hassan240@gmail.com>
2024-11-21 22:11:19 +0600

    feat: logger update and request response log

--- src/core/logger/logger.py
+++ src/core/logger/logger.py
@@ -1,65 +1,80 @@
-import datetime
 import json
-import logging
 from typing import Any
 
-import stackprinter
+import structlog
+from structlog import processors
+from structlog.processors import JSONRenderer
+from structlog.typing import EventDict
 
-from src.core.schemas.common import BaseJsonLogSchema
-
-LEVEL_TO_NAME = {
-    logging.CRITICAL: "Critical",
-    logging.ERROR: "Error",
-    logging.WARNING: "Warning",
-    logging.INFO: "Information",
-    logging.DEBUG: "Debug",
-    logging.NOTSET: "Trace",
-}
+from src.core.config import settings
 
 
-class JSONLogFormatter(logging.Formatter):
-    def format(self, record: logging.LogRecord, *args: Any, **kwargs: Any) -> str:
-        _ = args
-        _ = kwargs
-        log_object: dict[Any, Any] = self._format_log_object(record)
-        return json.dumps(log_object, ensure_ascii=True, indent=4)
+# Define custom processor to censor sensitive information in response data
+def censor_sensitive_data(_: Any, __: Any, event_dict: EventDict) -> EventDict:
+    response_body = event_dict.get("response_body")
+    request_body = event_dict.get("request_body")
 
-    @staticmethod
-    def _format_log_object(record: logging.LogRecord) -> dict[Any, Any]:
-        now = (
-            datetime.datetime.fromtimestamp(record.created)
-            .astimezone()
-            .replace(microsecond=0)
-            .isoformat()
-        )
-        message = record.getMessage()
-        duration = record.duration if hasattr(record, "duration") else record.msecs
+    if isinstance(response_body, list):
+        event_dict["response_body"] = response_body[1:2]
+    if isinstance(response_body, dict):
+        response_body = {
+            key: value
+            for key, value in response_body.items()
+            if key.lower() not in {"password"} and "token" not in key.lower()
+        }
+        event_dict["response_body"] = response_body
 
-        json_log_fields = BaseJsonLogSchema(
-            thread=record.process,
-            timestamp=now,
-            level_name=LEVEL_TO_NAME[record.levelno],
-            message=message,
-            source_log=record.name,
-            duration=duration,
-        )
-        if hasattr(record, "props"):
-            json_log_fields.props = record.props
+    if isinstance(request_body, dict):
+        request_body["password"] = "censored"
 
-        if record.exc_info:
-            json_log_fields.exceptions = stackprinter.format(
-                record.exc_info,
-                suppressed_paths=[r"lib/python.*"],
-                add_summary=False,
-            ).split("\n")
+    if event_dict.get("request_header", {}).get("authorization"):
+        event_dict["request_header"]["authorization"] = "censored"
 
-        elif record.exc_text:
-            json_log_fields.exceptions = record.exc_text
+    return event_dict
 
-        json_log_object = json_log_fields.model_dump(
-            exclude_unset=True,
-            by_alias=True,
-        )
-        if hasattr(record, "request_json_fields"):
-            json_log_object.update(record.request_json_fields)
-        return json_log_object
+
+shared_processors: list[Any] = [
+    processors.add_log_level,
+    processors.StackInfoRenderer(),
+    processors.TimeStamper(fmt="iso"),
+    processors.EventRenamer("msg"),
+    processors.CallsiteParameterAdder(
+        [
+            processors.CallsiteParameter.FUNC_NAME,
+            processors.CallsiteParameter.LINENO,
+            processors.CallsiteParameter.FILENAME,
+        ]
+    ),
+    censor_sensitive_data,
+]
+
+console_log_formatter = structlog.dev.ConsoleRenderer()
+json_log_formatter = JSONRenderer(serializer=json.dumps, indent=2)
+
+
+def configure_console_logger() -> Any:
+    return structlog.wrap_logger(
+        structlog.get_logger(),
+        processors=[*shared_processors, console_log_formatter],
+    )
+
+
+def configure_json_logger() -> Any:
+    return structlog.wrap_logger(
+        structlog.get_logger(),
+        processors=[
+            *shared_processors,
+            structlog.processors.format_exc_info,
+            structlog.processors.dict_tracebacks,
+            json_log_formatter,
+        ],
+    )
+
+
+def configure_logger() -> Any:
+    if settings.APP_ENV == "dev":
+        return configure_console_logger()
+    return configure_json_logger()
+
+
+logger = configure_logger()
