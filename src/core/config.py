3945a282ed66b3fe9b5038b9e50418d34e6f9746
MahmudulHassan5809 <mahmudul.hassan240@gmail.com>
2025-07-01 21:35:56 +0600

    jwt handler class

--- src/core/config.py
+++ src/core/config.py
@@ -8,6 +8,10 @@ class Settings(BaseSettings):
     DATABASE_URL: str
     REDIS_URL: str
     PERSIST_REDIS_URL: str
+    JWT_SECRET: str
+    JWT_ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+    REFRESH_TOKEN_EXPIRE_MINUTES: int
 
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
